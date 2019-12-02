7869b03655322eb36e2bb5b3444b0aaea4779aa3
CK <kainingc@usc.edu>
2019-12-02 15:12:32 -0800

    add bidirectional BFS

--- src/com/CK/Main.java
+++ src/com/CK/Main.java
@@ -15,7 +15,7 @@ public class Main {
     }
 }
 
-class Solution {
+class Solution2 {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         Set<String> wordSet = new HashSet<>(wordList);
         if (!wordSet.contains(endWord))
@@ -42,6 +42,51 @@ class Solution {
                 }
             }
             ++res;
+        }
+        return 0;
+    }
+}
+
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> dict = new HashSet<>(wordList);
+        if (!dict.contains(endWord)) return 0;
+
+        Set<String> beginSet = new HashSet<>();
+        beginSet.add(beginWord);
+        dict.remove(beginWord);
+
+        Set<String> endSet = new HashSet<>();
+        endSet.add(endWord);
+        dict.remove(endWord);
+
+        int level = 1;
+        while (!beginSet.isEmpty() && !endSet.isEmpty()) {
+            Set<String> currentSet = (beginSet.size() < endSet.size()) ? beginSet : endSet;
+            Set<String> oppositeSet = (beginSet.size() >= endSet.size()) ? beginSet : endSet;
+            Set<String> temp = new HashSet<>();
+
+            for (String s : currentSet) {
+                for (int i = 0; i < s.length(); i++) {
+                    for (int j = 0; j < 26; j++) {
+                        char c = (char) (j + 'a');
+                        if (s.charAt(i) == c)
+                            continue;
+                        String next = s.substring(0, i) + c + s.substring(i + 1);
+                        if (oppositeSet.contains(next))
+                            return level + 1;
+
+                        if (dict.contains(next)) {
+                            temp.add(next);
+                            dict.remove(next);
+                        }
+                    }
+                }
+            }
+            currentSet.clear();
+            currentSet.addAll(temp);
+
+            level++;
         }
         return 0;
     }
